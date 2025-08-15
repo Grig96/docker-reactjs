81024c880fe7aea2d1ec405f6a08b649fe5ef356
Grigor Grigoryan <51883286+Grig96@users.noreply.github.com>
2025-08-15 20:55:59 +0400

    Update main-container.js

--- src/components/main-container.js
+++ src/components/main-container.js
@@ -30,11 +30,11 @@ export default class MainContainer extends Component {
       
       let data = result.data;
       let coins = [];
-      Object.keys(data).map((k,i) => {
-        if(data[k].last){
+      Object.keys(data).forEach((k, i) => {
+        if (data[k].last) {
           coins.push(data[k]);
         }
-      });
+    });
       this.setState({coins:coins});
     });
   }
